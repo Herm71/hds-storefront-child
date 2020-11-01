e929b66dacff4775240885c151143610109deade
Jason Chafin <Herm71@users.noreply.github.com>
2020-11-01 09:32:24 -0800

    chore: :bulb: Update TODOs

--- functions.php
+++ functions.php
@@ -12,7 +12,6 @@
 
  //TODO: #14 Sidebar Template
  //TODO: #15 Style Checkout page
- //TODO: #16 Style Shopping cart page
  //TODO: Style Top bar nav drop-down
  //TODO: All Hex colors to variables
  //TODO: Style Widget Titles
